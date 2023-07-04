fa3ad3c27488872536c5d34c1107f9afbf341174
AlexRado <kia8247@gmail.com>
2023-07-04 12:03:28 +0300

    add new button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import TodoItem from './TodoItem';
 import '../style/TodoList.css';
+import { removeTodo } from '../store/todosSlice';
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos); // Отримання списку елементів зі стану Redux
+  const dispatch = useDispatch();
+  const handleRemoveTodo = () => {
+    dispatch(removeTodo(todos));
+    console.log(todos);
+  };
 
   return (
     <ul>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} /> // Відображення кожного елемента списку
       ))}
+
+      <button style={{ marginTop: '10px' }} onClick={handleRemoveTodo}>
+        Remove Todo
+      </button>
     </ul>
   );
 };
--- src/store/todosSlice.js
+++ src/store/todosSlice.js
@@ -1,24 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Створення "срізу" (slice) за допомогою функції createSlice з пакету @reduxjs/toolkit
 const todosSlice = createSlice({
-  name: 'todos',
-  initialState: [],
+  name: 'todos', // Назва срізу
+  initialState: [], // Початковий стан (пустий масив)
   reducers: {
+    // Визначення редукторів (actions) для зміни стану срізу
     addTodo: (state, action) => {
-      state.push({ id: Date.now(), text: action.payload, completed: false });
+      // Редуктор для додавання нової задачі
+      state.push({ id: Date.now(), text: action.payload, completed: false }); // Додавання нового об'єкта задачі до стану масиву
     },
     toggleTodo: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload);
+      // Редуктор для зміни статусу виконання задачі
+      const todo = state.find((todo) => todo.id === action.payload); // Знаходження задачі за ідентифікатором
       if (todo) {
-        todo.completed = !todo.completed;
+        // Якщо задача знайдена
+        todo.completed = !todo.completed; // Зміна статусу виконання задачі
       }
     },
     deleteTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload);
+      // Редуктор для видалення задачі
+      return state.filter((todo) => todo.id !== action.payload); // Фільтрація масиву задач за ідентифікатором і повернення нового масиву без видаленої задачі
     },
+    removeTodo: () => [],
   },
 });
 
-export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+// Експорт редукторів (actions) з срізу
+export const { addTodo, toggleTodo, deleteTodo, removeTodo } =
+  todosSlice.actions;
 
+// Експорт редуктора (reducer) срізу
 export default todosSlice.reducer;
