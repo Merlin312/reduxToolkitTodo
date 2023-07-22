185852976851d9bbe8c9b109b7db95be6f88fe59
AlexRado <kia8247@gmail.com>
2023-07-22 14:13:04 +0300

    added new button UpperCase

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,13 @@
 import RemoveAll from './buttons/RemoveAll';
 import SortTodo from './buttons/SortTodo';
+import UpperCase from './buttons/UpperCase';
 
 const Buttons = () => {
   return (
     <div>
       <RemoveAll />
       <SortTodo />
+      <UpperCase />
     </div>
   );
 };
---
+++ src/components/buttons/UpperCase.jsx
@@ -0,0 +1,8 @@
+const UpperCase = () => {
+  return (
+    <>
+      <button onClick={handleUpperCase}>UpperCase</button>
+    </>
+  );
+};
+export default UpperCase;
