d85c05229b0710c728ed34dc10acb29e4c45b026
AlexRado <kia8247@gmail.com>
2024-05-09 22:06:20 +0300

    second practice use methods

--- src/PracticeApp.jsx
+++ src/PracticeApp.jsx
@@ -1,45 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const PracticeApp = () => {
   const [todo, setTodo] = useState([]);
   const [value, setValue] = useState('');
-
-  useEffect(() => {
-    const interval1 = setInterval(() => {
-      console.clear();
-    }, 60000);
-    return () => {
-      clearInterval(interval1);
-    };
-  }, []);
+  // const [showButon, setShowButton] = useState(false);
   const handleValue = (event) => {
-    setValue(event.target.value);
+    event.target.value;
   };
-  const handleAddValue = () => {
+  const handleAddNewTask = () => {
     setTodo([...todo, value]);
     setValue('');
   };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.clear();
+    }, 30000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  // useEffect(() => {
+  //   const renderInterval = setTimeout(() => {
+  //     setShowButton(true);
+  //     return () => {
+  //       clearTimeout(renderInterval);
+  //     };
+  //   }, 10000);
+  // }, []);
   return (
     <div>
-      <input value={value} onChange={handleValue} />
-      <button onClick={handleAddValue}>Add todo</button>
+      {/* {showButon && <button>setTimeButton</button>} */}
+      <input
+        value={value}
+        onChange={handleValue}
+        placeholder="write your todos"
+        style={{
+          fontSize: '1.5em',
+          borderRadius: '0.5em',
+          border: '0.1em solid white',
+          padding: '0.2em',
+          marginRight: '0.5em',
+        }}
+      />
+      <button onClick={handleAddNewTask}>Add new task</button>
+      <h1>List of todos:</h1>
       <ul>
-        {todo.map((todoItem, index) => {
-          return (
-            <li
-              style={{
-                color: 'red',
-                fontSize: '2em',
-              }}
-              key={index}
-            >
-              {todoItem}
-            </li>
-          );
+        {todo.map((todos, index) => {
+          return <li key={index}>{todos}</li>;
         })}
       </ul>
     </div>
   );
 };
-
 export default PracticeApp;
