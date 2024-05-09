c14f71f99ec8d7ab37e5e2f4f8df67bd5603b414
AlexRado <kia8247@gmail.com>
2024-05-09 09:03:11 +0300

    practice to coding

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="container">
       <Buttons />
-      <h1>Todo List! </h1>
+      <h1>Todo List!!! </h1>
       <TodoForm />
       <TodoList />
     </div>
