6f9bfa84cf644a1dd6ff49b597790344e5759e74
AlexRado <kia8247@gmail.com>
2024-05-10 11:23:59 +0300

    practice with modal

--- src/PracticeApp.jsx
+++ src/PracticeApp.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import Modal from './practice/Modal';
 import Todo from './practice/todo/Todo';
 
 const PracticeApp = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <Todo />
+      <h1>Simple Modal Example</h1>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 };
--- src/practice/Modal.jsx
+++ src/practice/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Modal.css';
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
@@ -6,11 +7,14 @@ const Modal = ({ isOpen, onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
+        <button>dddd</button>
+
         <button className="close-button" onClick={onClose}>
           Close
         </button>
         <h2>Modal Title</h2>
         <p>This is the content of the modal.</p>
+        <button>dddd</button>
       </div>
     </div>
   );
